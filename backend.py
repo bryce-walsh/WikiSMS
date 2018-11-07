55cc4948d2f512a4f0ceccfff9f69cf1382e6bff
bryce-walsh <bryce.j.walsh@gmail.com>
2018-11-06 22:55:07 -0500

    Added basic support for scraping from sidebar

--- backend.py
+++ backend.py
@@ -1,4 +1,5 @@
 from mediawiki import MediaWiki
+from bs4 import BeautifulSoup
 
 wikipedia = MediaWiki()
 
@@ -7,9 +8,31 @@ def get_first_160(query):
   page = wikipedia.page(query)
   return page.summarize(chars=160)
 
+#Searches the sidebar of the page with the given title and 
+#Returns the string associated with the hint if one is found
+def check_sidebar(title, hint):
+	page = wikipedia.page(title)
+
+	soup = BeautifulSoup(page.html, 'html.parser')
+	info = soup.find('table', {'class': 'infobox'})
+	#Inspired by the answer on:
+	#https://stackoverflow.com/questions/23377533/python-beautifulsoup-parsing-table
+	info_body = info.find('tbody')
+	rows = info_body.find_all("tr")
+	infobox = {}
+	for row in rows:
+		parameter = row.find('th')
+		if parameter != None:
+			values = row.find_all('td')
+			parameter = parameter.text.strip()
+			values = [ele.text.strip() for ele in values]
+			infobox[parameter] = values
+	#print(infobox)
+	for key in infobox.keys():
+		if hint in key:
+			return infobox[key]
+
 #Scrapes Wikipedia for a 160 char response
 def scrape_wiki(title, type_, category, hint):
-  return get_first_160(title)
+  return check_sidebar(title,hint)
 
-
-#print(get_response("Tufts University"))
