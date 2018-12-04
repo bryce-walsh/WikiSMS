af9368cb10be7053f796bda0d69a1d52332e82ab
bryce-walsh <bryce.j.walsh@gmail.com>
2018-12-04 17:07:33 -0500

    fix parsing_constants commit

---
+++ parsing_constants.py
@@ -0,0 +1,3 @@
+#Constants for the wikipedia parsing backend
+RESPONSE_LEN = 160 # Length in characters of the response when searching the main text.
+MIDDLE_LEN = 20 # Length in characters of the window to be considered the "middle" of a response
